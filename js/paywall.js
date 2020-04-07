e82f565c5139da380b867340575dec9c664bd672
Stef Naskova <snaskova094@gmail.com>
2020-04-07 16:46:53 +0200

    Added: Responsive iframe code

--- js/paywall.js
+++ js/paywall.js
@@ -5,17 +5,17 @@ var paywall = new InplayerPaywall("9981d40b-f442-47c8-a177-9a74ca0c83a3", [
 $(function () {
 	$(".inplayer-paywall-logout").hide();
 
-	paywall.on("authenticated", function () {
+	paywall.on("authenticated", () => {
 		$(".inplayer-paywall-login").hide();
 		$(".inplayer-paywall-logout").show();
 	});
 
-	paywall.on("logout", function () {
+	paywall.on("logout", () => {
 		location.reload();
 	});
 
-	paywall.on("payment", function (a, b) {
-		// console.log(a,b)
-		window.location.replace("videos.html");
+	paywall.on('inject', () => {
+		$('.inplayer-paywall').addClass('video-wrapper');
 	});
+
 });
